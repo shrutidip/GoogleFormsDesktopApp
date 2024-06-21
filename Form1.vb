e9d5eee99784f9fe689191e558ccc8373d03f918
Shruti <67967323+shrutidip@users.noreply.github.com>
2024-06-21 22:47:50 +0530

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class MainForm
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialize form
+    End Sub
+
+    Private Sub btnViewSubmissions_Click(sender As Object, e As EventArgs) Handles btnViewSubmissions.Click
+        Dim viewSubmissionsForm As New ViewSubmissionsForm()
+        viewSubmissionsForm.ShowDialog()
+    End Sub
+
+    Private Sub btnCreateNewSubmission_Click(sender As Object, e As EventArgs) Handles btnCreateNewSubmission.Click
+        Dim createSubmissionForm As New CreateSubmissionForm()
+        createSubmissionForm.ShowDialog()
+    End Sub
+
+End Class
